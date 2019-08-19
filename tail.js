884cf4a4d65903ce7cb3bd9e6f2cb2fb2e0c9b5c
Jonathan <j.milne21@gmail.com>
2019-08-19 19:54:27 +0000

    New file tail.js

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  const affirmativeEmoji = String.fromCodePoint(0x1F60E);
+  const negativeEmoji = String.fromCodePoint(0x274C);
+  const affirmativeMessage = `${affirmativeEmoji} Assertion Passed: ${actual} === ${expected}`;
+  const negativeMessage = `${negativeEmoji} Assertion Failed: ${actual} !== ${expected}`;
+  return (actual === expected) ? console.log(affirmativeMessage) : console.log(negativeMessage);
+};
+
+const tail = function(array) {
+  let tailArray = array.slice(1);
+  return tailArray;
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+assertEqual(tail(words).length, 3);
