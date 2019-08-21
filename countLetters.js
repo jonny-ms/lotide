0cbc8a8bc323db5022ee3303561fb4888987d3c2
Jonathan <j.milne21@gmail.com>
2019-08-21 16:57:22 +0000

    counting letters

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  const affirmativeEmoji = String.fromCodePoint(0x1F60E);
+  const negativeEmoji = String.fromCodePoint(0x274C);
+  const affirmativeMessage = `${affirmativeEmoji} Assertion Passed: ${actual} === ${expected}`;
+  const negativeMessage = `${negativeEmoji} Assertion Failed: ${actual} !== ${expected}`;
+  return (actual === expected) ? console.log(affirmativeMessage) : console.log(negativeMessage);
+};
+
+const countLetters = function(str) {
+  let result = {};
+  for (const letter of str) {
+    if (letter !== " ") {
+      if (result[letter]) {
+        result[letter] += 1;
+      } else {
+        result[letter] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+// TEST CODE
+// console.log(countLetters("lighthouse in the house"));
