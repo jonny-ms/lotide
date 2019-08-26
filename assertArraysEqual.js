b709cfb357e89e6da28571e5a6d5cfe6d65d80ac
Jonathan <j.milne21@gmail.com>
2019-08-26 14:16:23 +0000

    clean up and function export

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,20 +1,12 @@
+const eqArrays = require("./eqArrays")
+
 const assertArraysEqual = function(array1, array2) {
   const affirmativeEmoji = String.fromCodePoint(0x1F60E);
   const negativeEmoji = String.fromCodePoint(0x274C);
   const pass = `${affirmativeEmoji} Assertion Passed: ${array1} === ${array2}`;
   const fail = `${negativeEmoji} Assertion Failed: ${array1} !== ${array2}`;
-
-  if (array1.length !== array2.length) {
-    return console.log(fail);
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) return console.log(fail);
-    }
-    return console.log(pass);
-  }
+  if (eqArrays(array1, array2)) console.log(pass);
+  else console.log(fail);
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+module.exports = assertArraysEqual;
