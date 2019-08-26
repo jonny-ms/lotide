4753843af09b46b96c114b11d8251fc9b0e005ad
Jonathan <j.milne21@gmail.com>
2019-08-26 13:36:42 +0000

    clean up of head.js and assertEqual.js by seperating out test code and exporting functions

--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  const affirmativeEmoji = String.fromCodePoint(0x1F60E);
-  const negativeEmoji = String.fromCodePoint(0x274C);
-  const affirmativeMessage = `${affirmativeEmoji} Assertion Passed: ${actual} === ${expected}`;
-  const negativeMessage = `${negativeEmoji} Assertion Failed: ${actual} !== ${expected}`;
-  return (actual === expected) ? console.log(affirmativeMessage) : console.log(negativeMessage);
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head("string", "s"));
-assertEqual(head([]), "string");
+module.exports = head;
