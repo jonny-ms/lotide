14ee9c8a227a75b119db7af44ae2e9d30ae4496e
Jonathan <j.milne21@gmail.com>
2019-08-26 15:32:06 +0000

    middle.js cleaned up, function exported, middleTest.js created

--- middle.js
+++ middle.js
@@ -1,30 +1,3 @@
-const assertArraysEqual = function(array1, array2) {
-  const affirmativeEmoji = String.fromCodePoint(0x1F60E);
-  const negativeEmoji = String.fromCodePoint(0x274C);
-  const pass = `${affirmativeEmoji} Assertion Passed: ${array1} === ${array2}`;
-  const fail = `${negativeEmoji} Assertion Failed: ${array1} !== ${array2}`;
-
-  if (array1.length !== array2.length) {
-    return console.log(fail);
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) return console.log(fail);
-    }
-    return console.log(pass);
-  }
-};
-
-const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) return false;
-    }
-    return true;
-  }
-};
-
 const middle = function(arr) {
   let output = [];
   if (arr.length < 3) {
@@ -37,7 +10,5 @@ const middle = function(arr) {
   return output;
 };
 
-//TEST CODE
-console.log(middle([1, 2]));
-console.log(middle([1, 2, 3, 4, 5]));
-console.log(middle([1, 2, 3, 4]));
+
+module.exports = middle;
